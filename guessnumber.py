410079dfef5a670665fbe14f1e10a2388cde205b
PrajwalMendon <64822182+PrajwalMendon@users.noreply.github.com>
2020-05-09 19:34:31 -0700

    Add files via upload

---
+++ guessnumber.py
@@ -0,0 +1,24 @@
+# generate a random number unknown to user
+# user inputs a guess
+# program informs user if guess is too high or too low
+
+import random
+
+def guessnumber():
+    answer = random.randint(0, 100)
+    guess = int(input('Guess a number: '))
+    guessagain = 'y'
+    while guessagain == 'y':
+        if guess == answer:
+            print('You guessed correctly!')
+            guessagain = 'n'
+        elif guess > answer:
+            print('You guessed too high!')
+            guessagain = input('Guess again (Y/N)? ').lower()
+            guess = int(input('Guess a number: '))
+        else:
+            print('You guessed too low!')
+            guessagain = input('Guess again (Y/N)? ').lower()
+            guess = int(input('Guess a number: '))
+
+guessnumber()
