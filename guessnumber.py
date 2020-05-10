a8f233fe22024073ebd9f2c7b4205fb67bad1003
PrajwalMendon <64822182+PrajwalMendon@users.noreply.github.com>
2020-05-09 23:52:34 -0700

    Update guessnumber.py

--- guessnumber.py
+++ guessnumber.py
@@ -4,9 +4,20 @@
 
 import random
 
+def inputNumber(message):  # totes got this off the web since I had no clue how to do this on my own :(
+    while True:
+        try:
+            userInput = int(input(message))
+        except ValueError:
+            print('That is not a number!')
+            continue
+        else:
+            return userInput
+        break
+
 def guessnumber():
     answer = random.randint(0, 100)
-    guess = int(input('Guess a number: '))
+    guess = inputNumber('Guess a number: ')
     guessagain = 'y'
     while guessagain == 'y':
         if guess == answer:
